857e9fdf1ba74c34448878bf9d7a8032527aee7e
shadril238 <shadrilhassan@outlook.com>
2023-05-01 03:08:58 +0600

    BLL - Services

---
+++ DispensaryTrack/BLL/Services/BillService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DispensaryTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class BillService
+    {
+        public static List<BillDTO> Get()
+        {
+            var data = DataAccessFactory.BillData().Get();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Bill, BillDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<BillDTO>>(data);
+            return mapped;
+        }
+        public static BillDTO Get(int id)
+        {
+            var data = DataAccessFactory.BillData().Get(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Bill, BillDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<BillDTO>(data);
+            return mapped;
+        }
+        public static bool Create(BillDTO bill)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<BillDTO, Bill>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<Bill>(bill);
+            return DataAccessFactory.BillData().Insert(mapped);
+        }
+        public static bool Update(BillDTO bill)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<BillDTO, Bill>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<Bill>(bill);
+            return DataAccessFactory.BillData().Update(mapped);
+        }
+        public static bool Delete(int id)
+        {
+            return DataAccessFactory.BillData().Delete(id);
+        }
+    }
+}
--- DispensaryTrack/BLL/Services/EmployeeService.cs
+++ DispensaryTrack/BLL/Services/EmployeeService.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿//shadril238
+using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DispensaryTrack.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Services
 {
-    internal class EmployeeService
+    public class EmployeeService
     {
+        public static List<EmployeeDTO> Get()
+        {
+            var data = DataAccessFactory.EmployeeData().Get();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Employee, EmployeeDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<EmployeeDTO>>(data);
+            return mapped;
+        }
+        public static EmployeeDTO Get(int id)
+        {
+            var data = DataAccessFactory.EmployeeData().Get(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Employee, EmployeeDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<EmployeeDTO>(data);
+            return mapped;
+        }
+        public static bool Create(EmployeeDTO employee)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<EmployeeDTO, Employee>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<Employee>(employee);
+            return DataAccessFactory.EmployeeData().Insert(mapped);
+        }
+        public static bool Update(EmployeeDTO employee)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<EmployeeDTO, Employee>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<Employee>(employee);
+            return DataAccessFactory.EmployeeData().Update(mapped);
+        }
+        public static bool Delete(int id)
+        {
+            return DataAccessFactory.EmployeeData().Delete(id);
+        }
+        
     }
 }
