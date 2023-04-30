c0b26b31c514d873e1a49b6c5bfb43abb93bb94a
shadril238 <shadrilhassan@outlook.com>
2023-05-01 02:00:40 +0600

    DAL - DataAccessFactory

--- DispensaryTrack/DAL/DataAccessFactory.cs
+++ DispensaryTrack/DAL/DataAccessFactory.cs
@@ -12,30 +12,37 @@ namespace DAL
 {
     public class DataAccessFactory
     {
+        //Bill
         public static IRepo<Bill, int, bool> BillData()
         {
             return new BillRepo();
         }
+        //Category
         public static IRepo<Category, int, bool> CategoryData()
         {
             return new CategoryRepo();
         }
+        //Customer
         public static IRepo<Customer, int, bool> CustomerData()
         {
             return new CustomerRepo();
         }
+        //Employee
         public static IRepo<Employee, int, bool> EmployeeData()
         {
             return new EmployeeRepo();
         }
+        //Medicine
         public static IRepo<Medicine, int , bool> MedicineData()
         {
             return new MedicineRepo();
         }
+        //Order
         public static IRepo<Order, int , bool> OrderData()
         {
             return new OrderRepo();
         }
+        //OrderDetail
         public static IRepo<OrderDetail, int, bool> OrderDetailData()
         {
             return new OrderDetailRepo();
