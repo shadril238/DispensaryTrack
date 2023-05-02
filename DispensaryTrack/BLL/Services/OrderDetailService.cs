68ae1d493ae39caae7fef4a9de92235a161db0aa
shadril238 <shadrilhassan@outlook.com>
2023-05-02 14:23:47 +0600

    BLL - Services, AppLayer - Controllers [for OrderDetail]

---
+++ DispensaryTrack/BLL/Services/OrderDetailService.cs
@@ -0,0 +1,63 @@
+﻿//shadril238
+using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DispensaryTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class OrderDetailService
+    {
+        public static List<OrderDetailDTO> Get()
+        {
+            var data = DataAccessFactory.OrderDetailData().Get();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<OrderDetail, OrderDetailDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<OrderDetailDTO>>(data);
+            return mapped;
+        }
+        public static OrderDetailDTO Get(int id)
+        {
+            var data = DataAccessFactory.OrderDetailData().Get(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<OrderDetail, OrderDetailDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<OrderDetailDTO>(data);
+            return mapped;
+        }
+        public static bool Create(OrderDetailDTO orderdetail)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<OrderDetailDTO, OrderDetail>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<OrderDetail>(orderdetail);
+            return DataAccessFactory.OrderDetailData().Insert(mapped);
+        }
+        public static bool Update(OrderDetailDTO orderdetail)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<OrderDetailDTO, OrderDetail>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<OrderDetail>(orderdetail);
+            return DataAccessFactory.OrderDetailData().Update(mapped);
+        }
+        public static bool Delete(int id)
+        {
+            return DataAccessFactory.OrderDetailData().Delete(id);
+        }
+    }
+}
---
+++ DispensaryTrack/DispensaryTrack/Controllers/OrderDetailController.cs
@@ -0,0 +1,85 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DispensaryTrack.Controllers
+{
+    public class OrderDetailController : ApiController
+    {
+        [HttpGet]
+        [Route("api/orderdetails")]
+        public HttpResponseMessage OrderDetails()
+        {
+            try
+            {
+                var data = OrderDetailService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("api/orderdetails/{id}")]
+        public HttpResponseMessage OrderDetail(int id)
+        {
+            try
+            {
+                var data = OrderDetailService.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/orderdetails/delete/{id}")]
+        public HttpResponseMessage DeleteOrderDetail(int id)
+        {
+            try
+            {
+                var data = OrderDetailService.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/orderdetails/insert")]
+        public HttpResponseMessage InsertOrderDetail(OrderDetailDTO orderdetail)
+        {
+            try
+            {
+                var data = OrderDetailService.Create(orderdetail);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/orderdetails/update")]
+        public HttpResponseMessage UpdateOrderDetail(OrderDetailDTO orderdetail)
+        {
+            try
+            {
+                var data = OrderDetailService.Update(orderdetail);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+        }
+    }
+}
